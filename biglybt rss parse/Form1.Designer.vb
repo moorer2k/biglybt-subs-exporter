0b7431dcf6149e82d4c6eb58ebd2734197498cc0
Robert Moore <moorer2k@gmail.com>
2021-04-03 13:52:52 -0700

    Initial commit.

---
+++ biglybt rss parse/Form1.Designer.vb
@@ -0,0 +1,105 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ButtonExport = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextSubsDir = New System.Windows.Forms.TextBox()
+        Me.ButtonBrowse = New System.Windows.Forms.Button()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.SuspendLayout()
+        '
+        'ButtonExport
+        '
+        Me.ButtonExport.Enabled = False
+        Me.ButtonExport.Location = New System.Drawing.Point(110, 45)
+        Me.ButtonExport.Name = "ButtonExport"
+        Me.ButtonExport.Size = New System.Drawing.Size(155, 23)
+        Me.ButtonExport.TabIndex = 0
+        Me.ButtonExport.Text = "Export"
+        Me.ButtonExport.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(12, 85)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(282, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Status: Idle.."
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 20)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(92, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "BiglyBT Subs Dir:"
+        '
+        'TextSubsDir
+        '
+        Me.TextSubsDir.Location = New System.Drawing.Point(110, 17)
+        Me.TextSubsDir.Name = "TextSubsDir"
+        Me.TextSubsDir.Size = New System.Drawing.Size(155, 22)
+        Me.TextSubsDir.TabIndex = 3
+        '
+        'ButtonBrowse
+        '
+        Me.ButtonBrowse.Location = New System.Drawing.Point(271, 17)
+        Me.ButtonBrowse.Name = "ButtonBrowse"
+        Me.ButtonBrowse.Size = New System.Drawing.Size(26, 22)
+        Me.ButtonBrowse.TabIndex = 4
+        Me.ButtonBrowse.Text = "..."
+        Me.ButtonBrowse.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(306, 107)
+        Me.Controls.Add(Me.ButtonBrowse)
+        Me.Controls.Add(Me.TextSubsDir)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ButtonExport)
+        Me.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "BitlyBT Subs Exporter"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ButtonExport As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextSubsDir As TextBox
+    Friend WithEvents ButtonBrowse As Button
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+End Class
---
+++ biglybt rss parse/Form1.vb
@@ -0,0 +1,111 @@
+﻿Imports System.IO
+Imports System.Net
+Imports System.Text
+Imports System.Text.RegularExpressions
+
+Public Class Form1
+
+    Private ReadOnly rssLinks As New Hashtable
+    Private ReadOnly magnetLinks As New Hashtable
+
+    Sub parseMagnet(data As String)
+        data = data.Replace("\/", "/")
+        Dim regMagnet As Regex = New Regex("(magnet:\?xt=urn:btih:.*?)\""")
+        Dim intC As Integer = 0
+        If regMagnet.IsMatch(data) Then
+            For Each match As Match In regMagnet.Matches(data)
+                intC += 1
+                Dim magLink = WebUtility.UrlDecode(match.Groups(1).Value)
+                If Not magnetLinks.ContainsKey(magLink) Then
+                    magnetLinks.Add(magLink, intC)
+                End If
+            Next
+        End If
+    End Sub
+
+    Sub parseDecodeRSS(data As String)
+        data = data.Replace("\/", "/")
+        Dim intC As Integer = 0
+        Dim regURL As Regex = New Regex("\w+:\/\/[\w@][\w.:@]+\/?[\w\.?=%&=\-@/$,]*")
+        Dim regContent As Regex = New Regex("content"":""(.*)""}},")
+        If regContent.IsMatch(data) Then
+            Dim link = Encoding.UTF8.GetString(Convert.FromBase64String(regContent.Match(data).Groups(1).Value))
+            If regURL.IsMatch(link) Then
+                Dim rssLink = WebUtility.UrlDecode(regURL.Match(link).Value)
+                If Not rssLinks.ContainsKey(rssLink) Then
+                    intC += 1
+                    rssLinks.Add(rssLink, intC)
+                End If
+            End If
+        End If
+    End Sub
+
+    Private Sub ButtonExport_Click(sender As Object, e As EventArgs) Handles ButtonExport.Click
+
+        Dim subsDir As String = TextSubsDir.Text
+        ButtonExport.Enabled = False
+
+        Dim tThread As New Threading.Thread(Sub()
+
+                                                Dim files = Directory.GetFiles(subsDir)
+                                                Dim intCurrent As Integer = 0
+                                                Dim intTotal As Integer = files.Count
+
+                                                For Each fi In files
+                                                    Using sr As StreamReader = File.OpenText(fi)
+                                                        Dim sb As New StringBuilder()
+                                                        sb.Append(sr.ReadToEnd())
+                                                        If fi.Contains(".results") Then
+                                                            parseMagnet(sb.ToString)
+                                                        End If
+                                                        If fi.Contains(".biglybt") Then
+                                                            parseDecodeRSS(sb.ToString)
+                                                        End If
+                                                    End Using
+                                                    intCurrent += 1
+                                                    SetText(Me, Label1, $"Status: Parsing {intCurrent} of {intTotal}.")
+                                                Next
+
+                                                Using writer = File.AppendText(Application.StartupPath & "\magnetLinks.txt")
+                                                    For Each link In magnetLinks.Keys
+                                                        writer.WriteLine(link)
+                                                    Next link
+                                                End Using
+
+                                                Using writer = File.AppendText(Application.StartupPath & "\rssLinks.txt")
+                                                    For Each link In rssLinks.Keys
+                                                        writer.WriteLine(link)
+                                                    Next link
+                                                End Using
+
+                                                SetText(Me, Label1, $"Status: Exported {rssLinks.Count + magnetLinks.Count} links.")
+
+                                                rssLinks.Clear()
+                                                magnetLinks.Clear()
+
+                                            End Sub)
+
+        tThread.Start()
+
+    End Sub
+
+    Private Sub ButtonBrowse_Click(sender As Object, e As EventArgs) Handles ButtonBrowse.Click
+        FolderBrowserDialog1.Description = "Select the subs folder from exported backup directory."
+        FolderBrowserDialog1.ShowDialog()
+        If FolderBrowserDialog1.SelectedPath <> "" Then
+            TextSubsDir.Text = FolderBrowserDialog1.SelectedPath
+            ButtonExport.Enabled = True
+        End If
+    End Sub
+
+    Delegate Sub SetText_Delegate(Of T As {New, Control})([Form] As Form, [Control] As T, [Text] As String)
+    Public Sub SetText(Of T As {New, Control})([Form] As Form, [Control] As T, [Text] As String)
+        If [Control].InvokeRequired Then
+            Dim MyDelegate As New SetText_Delegate(Of T)(AddressOf SetText)
+            [Form].Invoke(MyDelegate, New Object() {[Form], [Control], [Text]})
+        Else
+            [Control].Text = [Text]
+        End If
+    End Sub
+
+End Class
